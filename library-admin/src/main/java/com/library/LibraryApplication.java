23ccbcde0dc703ae08e722a9a1fae9752bc9fecf
xiangziyang <xiangziyang@wxm2m.com>
2025-12-24 17:35:19 +0800

    refactor:改名

---
+++ library-admin/src/main/java/com/library/LibraryApplication.java
@@ -0,0 +1,21 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class LibraryApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(LibraryApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  图书管理系统启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+    }
+}
