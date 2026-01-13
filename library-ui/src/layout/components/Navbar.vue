1f729d00d8cb36ef16475ff82e55235e1fbba6d8
noname <noname@qq.com>
2026-01-13 16:26:35 +0800

    fix：修复退出登录无法显示首页

--- library-ui/src/layout/components/Navbar.vue
+++ library-ui/src/layout/components/Navbar.vue
@@ -105,7 +105,7 @@ export default {
         type: 'warning'
       }).then(() => {
         this.$store.dispatch('LogOut').then(() => {
-          location.href = '/index'
+          location.href = '/#/login?redirect=%2Findex'
         })
       }).catch(() => {})
     }
